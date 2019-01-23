26e303b2c68de316c0614e51a515b56dc0199761
Ralf Manuel Morawe <mowalle.dev@gmail.com>
2019-01-23 10:57:57 +0100

    Improve overview table of performance

--- result_analysis/Means.py
+++ result_analysis/Means.py
@@ -11,20 +11,24 @@ num_tasks = 6
 # Means per condition ----
 
 df = pd.read_csv(str(sys.argv[1]), delimiter=",")
-df = df[["conditionIndex", "megamapTime", "horizOffsetDeg", "vertOffsetDeg"]]
+df = df[["conditionIndex", "megamapTime", "horizOffsetDeg", "vertOffsetDeg", "hitRoom"]]
 
-meansPerCondition = []
+# output = pd.DataFrame(index="])
+values = []
 for condition in range(0, num_conditions):
     df_cond = df[df["conditionIndex"] == condition]
-    means = {
-        "conditionIndex": condition,
-        "Mean megamapTime": df_cond["megamapTime"].mean(),
-        "Mean horizOffsetDeg": df_cond["horizOffsetDeg"].mean(),
-        "Mean vertOffsetDeg": df_cond["vertOffsetDeg"].mean()}
-    meansPerCondition.append(means)
+    column = [df_cond["megamapTime"].mean(), df_cond["megamapTime"].std(),
+              df_cond["horizOffsetDeg"].mean(), df_cond["horizOffsetDeg"].std(),
+              df_cond["vertOffsetDeg"].mean(), df_cond["vertOffsetDeg"].std(),
+              df_cond["hitRoom"].mean() * 100]
+    values.append(column)
 
-print("Means per condition:")
-for row in meansPerCondition:
-    print(row)
-
+output = pd.DataFrame(values, index=["3D_l", "3D_h", "2D"], columns=["Megamap Time",
+                                                                           "std Time",
+                                                                           "H-Error",
+                                                                           "std H-Error",
+                                                                           "V-Error",
+                                                                           "std V-Error",
+                                                                           "Hitrate"]).transpose().round(2)
+print(output)
 # ---------------------------
